b9fb880b1385cf58ede47335163f11dea0d8c14c
Iven Krall <iven.krall@bewotec.de>
2018-09-18 11:51:48 +0200

    Initial version setup for fernice

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.fernice.reflare.ten {
+
+    requires java.desktop;
+    requires java.se;
+    requires fernice.reflare.access;
+
+    exports org.fernice.reflare.internal.impl;
+}
---
+++ src/main/java/org/fernice/reflare/internal/impl/AATextInfoAccessorImpl.java
@@ -0,0 +1,28 @@
+package org.fernice.reflare.internal.impl;
+
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.Locale;
+import java.util.Map;
+import org.fernice.reflare.internal.AATextInfoHelper.AATextInfoAccessor;
+import sun.swing.SwingUtilities2;
+
+public class AATextInfoAccessorImpl implements AATextInfoAccessor {
+
+    @Override
+    public void aaTextInfo(Map<Object, Object> defaults) {
+        SwingUtilities2.putAATextInfo(getAATextInfoCondition(), defaults);
+    }
+
+    private static boolean getAATextInfoCondition() {
+        final String language = Locale.getDefault().getLanguage();
+        final String desktop = AccessController.doPrivileged((PrivilegedAction<String>) () -> System.getProperty("sun.desktop"));
+
+        final boolean isCjkLocale = (Locale.CHINESE.getLanguage().equals(language) || Locale.JAPANESE.getLanguage().equals(language) ||
+                Locale.KOREAN.getLanguage().equals(language));
+        final boolean isGnome = "gnome".equals(desktop);
+        final boolean isLocal = SwingUtilities2.isLocalDisplay();
+
+        return isLocal && (!isGnome || !isCjkLocale);
+    }
+}
