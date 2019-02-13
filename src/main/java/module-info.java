8058167f97c56bcede1ead481343a9b24b0360f8
Iven Krall <iven.krall@bewotec.de>
2019-02-13 14:15:00 +0100

    Implemented SunFontAccessorImpl

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.fernice.reflare.ten {
     requires java.desktop;
     requires java.se;
     requires fernice.reflare.access;
+    requires annotations;
 
     exports org.fernice.reflare.internal.impl;
 }
